d22fa05b061dfad59f4c5b569a0acd38e14e101a
WonJ1yun <78727847+sudhdkso@users.noreply.github.com>
2024-02-24 00:08:24 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -4,17 +4,18 @@ class Solution {
         
         return findNext(n, Integer.bitCount(n));
     }
-    
+    //n과 n을 이진수로 변환했을 때 1의 갯수를 매개변수로 받아 다음 큰 수를 반환하는 함수
     private int findNext(int n, int oneCount){
         int next = n;
         
-        while(next <= 1_000_000){
+        while(true){
             next++;
+            //next를 이진수로 변환한 1의 갯수를 찾고
             int nextOneCount = Integer.bitCount(next);
+            //n의 1의 갯수와 같으면 바로 return
             if(oneCount == nextOneCount){
                 return next;
             }
         }
-        return 0;
     }
 }
