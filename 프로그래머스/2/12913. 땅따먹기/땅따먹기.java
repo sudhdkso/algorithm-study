78868ca4921e480323328220a1e8880f1316bffe
WonJ1yun <78727847+sudhdkso@users.noreply.github.com>
2024-06-17 18:09:15 +0900

    [level 2] Title: 땅따먹기, Time: 26.84 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,10 +1,20 @@
 class Solution {
     int solution(int[][] land) {
         int answer = 0;
+        int n = land.length, m = land[0].length;
+        
+        int[][] dp = new int[n][m];
+        for(int i=0;i<m;i++){
+            dp[0][i] = land[0][i];
+        }
+        
+        for(int i=1;i<n;i++){   
+            dp[i][0] = Math.max(dp[i-1][1], Math.max(dp[i-1][2],dp[i-1][3])) + land[i][0];
+            dp[i][1] = Math.max(dp[i-1][0], Math.max(dp[i-1][2],dp[i-1][3])) + land[i][1];
+            dp[i][2] = Math.max(dp[i-1][0], Math.max(dp[i-1][1],dp[i-1][3])) + land[i][2];
+            dp[i][3] = Math.max(dp[i-1][0], Math.max(dp[i-1][1],dp[i-1][2])) + land[i][3];
+        }
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+        return Math.max(dp[n-1][0],Math.max(dp[n-1][1], Math.max(dp[n-1][2], dp[n-1][3])));
     }
 }
