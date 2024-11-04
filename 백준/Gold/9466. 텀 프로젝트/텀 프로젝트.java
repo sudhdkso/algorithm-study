dcb6822975ed1603c7ddbc755b8be261a4f41d4e
WonJ1yun <78727847+sudhdkso@users.noreply.github.com>
2024-11-05 00:28:01 +0900

    [Gold III] Title: 텀 프로젝트, Time: 984 ms, Memory: 303900 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	private static int[] want;
-	private static boolean[] visited, isGroup;
+	private static boolean[] visited, isSearched;
 	private static int n;
 	private static int answer = 0;
 
@@ -22,18 +22,18 @@ public class Main {
 
 			visited = new boolean[n+1];
 			want = new int[n+1];
-			isGroup = new boolean[n+1];
+			isSearched = new boolean[n+1];
 
 			for (int i = 1; i <= n; i++) {
 				want[i] = Integer.parseInt(st.nextToken());
 				if(i == want[i]){
-					isGroup[i] = true;
+					isSearched[i] = true;
 					answer++;
 				}
 			}
 
 			for (int i = 1; i <= n; i++) {
-				if (isGroup[i]) continue;
+				if (isSearched[i]) continue;
 				dfs(i);
 			}
 
@@ -43,24 +43,15 @@ public class Main {
 	}
 
 	private static void dfs(int cur){
+		if(isSearched[cur]) return;
 		if(visited[cur]){
-			return;
+			isSearched[cur] = true;
+ 			answer++;
 		}
+
 		visited[cur] = true;
-		int next = want[cur];
-
-		if(!visited[next]){
-			dfs(next);
-		} else {
-			if(!isGroup[next]){
-				answer++;
-				while(next != cur){
-					answer++;
-					next = want[next];
-				}
-			}
-		}
-		isGroup[cur] = true;
-
+		dfs(want[cur]);
+		isSearched[cur] = true;
+		visited[cur] = false;
 	}
 }
