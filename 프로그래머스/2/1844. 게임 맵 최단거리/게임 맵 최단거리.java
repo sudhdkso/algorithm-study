cb642b7480990f52c6514dfc0b2ec5b852abb743
WonJ1yun <78727847+sudhdkso@users.noreply.github.com>
2025-01-10 08:01:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.08 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,51 +3,51 @@ import java.util.LinkedList;
 import java.util.Arrays;
 
 class Solution {
-    private class Point{
+    
+    private static class Point {
         int x,y;
         public Point(int x, int y){
             this.x = x;
             this.y = y;
         }
     }
-    private static int SX = 0, SY = 0;
+    
+    private static int[] dx = {-1,1,0,0}, dy = {0,0,-1,1};
+    
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        int answer = bfs(n, m, maps);
-        return answer == Integer.MAX_VALUE-1 ? -1 : answer;
+        int answer = 0;
+        return bfs(maps, maps.length, maps[0].length);
     }
-    private int[] dx = {0, 1, 0, -1}, dy = {1, 0 , -1, 0};
-    private int bfs(int n, int m, int[][] maps){
-        Queue<Point> q = new LinkedList<>();
-        
-        q.offer(new Point(SX,SY));
-        
+    
+    private static int bfs(int[][] map, int n, int m){
         int[][] dist = new int[n][m];
+        
+        Queue<Point> q = new LinkedList<>();
         
         for(int i=0;i<n;i++){
             Arrays.fill(dist[i], Integer.MAX_VALUE-1);
         }
         
-        dist[SX][SY] = 1;
+        dist[0][0] = 1;
+        q.offer(new Point(0,0));
+        
         while(!q.isEmpty()){
             Point now = q.poll();
+            if(now.x == n-1 && now.y == m-1){
+                return dist[now.x][now.y];
+            }
             
             for(int i=0;i<4;i++){
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
-                
                 if(nx >= 0 && ny >= 0 && nx < n && ny < m){
-                    if(maps[nx][ny] == 0) continue;
-                    if(dist[nx][ny] > dist[now.x][now.y] + 1) {
-                        dist[nx][ny] = dist[now.x][now.y]+1;
-                        q.offer(new Point(nx,ny));
+                    if(dist[nx][ny] > dist[now.x][now.y] + 1 && map[nx][ny] == 1){
+                        dist[nx][ny] = dist[now.x][now.y] + 1;
+                        q.offer(new Point(nx, ny));
                     }
                 }
             }
-            
         }
-        
-        return dist[n-1][m-1];
+        return -1;
     }
 }
