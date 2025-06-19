0ae9089e85adde5a30111b767755ba3897dd0a78
원지윤 <78727847+sudhdkso@users.noreply.github.com>
2025-06-20 00:29:58 +0900

    [level 3] Title: 베스트앨범, Time: 4.64 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,67 +1,54 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
-    private class Music implements Comparable<Music>{
-        int index, play;
-        public Music(int index, int play){
+    private static class Music implements Comparable<Music>{
+        int index, count;
+        
+        public Music(int index, int count){
             this.index = index;
-            this.play = play;
+            this.count = count;
         }
         
         @Override
-        public int compareTo(Music e){
-            if(Integer.compare(e.play, this.play) == 0){
-                return Integer.compare(this.index, e.index);
+        public int compareTo(Music obj){
+            if(this.count == obj.count){
+                return this.index-obj.index;
             }
-            return Integer.compare(e.play, this.play);
+            return obj.count-this.count;
         }
-        
     }
+    
     public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> totalPlay = new HashMap<>();
-        Map<String, PriorityQueue<Music>> genresMap = new HashMap<>();
         
-        int length = genres.length;
+        Map<String, Integer> musicCountMap = new HashMap<>();
+        Map<String,List<Music>> musicMap = new HashMap<>();
         
-        for(int i=0;i<length;i++){
-            String genre = genres[i];
-            totalPlay.put(genre, totalPlay.getOrDefault(genre,0)+plays[i]);
-            
-            if(!genresMap.containsKey(genre)){
-                genresMap.put(genre, new PriorityQueue<Music>());
+        //장르별 재생횟수 및 장르별 노래 분류
+        for(int i=0;i<genres.length;i++){
+            musicCountMap.put(genres[i], musicCountMap.getOrDefault(genres[i],0)+plays[i]);
+            if(musicMap.getOrDefault(genres[i], null) == null){
+                musicMap.put(genres[i], new ArrayList<Music>());
             }
-            genresMap.get(genre).offer(new Music(i, plays[i]));
+            
+            musicMap.get(genres[i]).add(new Music(i, plays[i]));
         }
         
-        List<String> sortedKey = sortGenresByPlayCount(totalPlay);
+        List<String> genresList = new ArrayList<>(musicCountMap.keySet());
         
-        return getBestAlbam(sortedKey, genresMap);
-    }
-    
-    private List<String> sortGenresByPlayCount(Map<String, Integer> totalPlay){
-        List<String> key = new ArrayList<>(totalPlay.keySet());
+        genresList.sort((o1,o2) -> musicCountMap.get(o2).compareTo(musicCountMap.get(o1)));
         
-        key.sort((o1,o2) -> {
-            return totalPlay.get(o2).compareTo(totalPlay.get(o1));
-        });
-        return key;
-    }
-    
-    private int[] getBestAlbam(List<String> sortedKey, Map<String, PriorityQueue<Music>> genreMap){
-        List<Integer> bestAlbam = new ArrayList<>();
-        
-        for(String key : sortedKey){
-            PriorityQueue<Music> pq = genreMap.get(key);
             
-            for(int i=0;i<2;i++){
-                if(pq.isEmpty()){
-                   break; 
-                }
-                bestAlbam.add(pq.poll().index);   
-            }
+        List<Integer> answer = new ArrayList<>();
+        
+        int index = 0;
+        
+        for(String key: genresList){
+            PriorityQueue<Music> pq = new PriorityQueue<>(musicMap.get(key));
+            answer.add(pq.poll().index);
+            if(pq.isEmpty()) continue;
+            answer.add(pq.poll().index);
         }
-        return bestAlbam.stream()
-            .mapToInt(i -> i)
-            .toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
