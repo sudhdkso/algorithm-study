43d98a6017b83a6b316a692cc736af5da35f9e30
원지윤 <78727847+sudhdkso@users.noreply.github.com>
2025-07-26 01:30:47 +0900

    [level 2] Title: 무인도 여행, Time: 26.13 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,75 +1,68 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Solution {
-    
-    private class Point implements Comparable<Point>{
-        int x,y;
+    static class Point {
+        int x, y;
         public Point(int x, int y){
             this.x = x;
             this.y = y;
         }
-        
-        @Override
-        public int compareTo(Point p){ 
-            if(Integer.compare(p.x, x) == 0){ 
-                 return Integer.compare(y,p.y);
-            }
-            return Integer.compare(x,p.x); 
-        }
     }
     
-    private boolean[][] visited;
-    private char SEA = 'X';
-    
-    private int[] dx = {1,0,-1,0}, dy = {0,1,0,-1};
+    static String SEA = "X";
+    static int[] dx = {1,0,-1,0}, dy = {0,1,0,-1};
     public int[] solution(String[] maps) {
         int[] answer = {};
-        int n = maps.length;
-        int m = maps[0].length();
+        int n = maps.length, m = maps[0].length();
+        
+        boolean[][] visited = new boolean[n][m];
+        String[][] strMaps = new String[n][m];
+        
+        for(int i=0;i<n;i++){
+            strMaps[i] = maps[i].split("");
+        }
+        
         List<Integer> list = new ArrayList<>();
         
-        visited =new boolean[n][m];
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j< m; j++){  
-               if(maps[i].charAt(j) != SEA && !visited[i][j]){
-                   int sum = bfs(i,j,n,m,maps);
-                   list.add(sum);
-               }
-
-            }
-        }
-        if(list.size() == 0){
-            list.add(-1);
-        }
-        Collections.sort(list);
-        return list.stream().mapToInt(Integer :: intValue).toArray();
-    }
-    
-    
-    public int bfs(int x, int y, int n, int m, String[] maps){
-       
-        int sum = Integer.parseInt(String.valueOf(maps[x].charAt(y)));
-        Queue<Point> q = new LinkedList<>();
-        visited[x][y] = true;
-        q.offer(new Point(x,y));
-        
-        while(!q.isEmpty()){   
-            Point p = q.poll();
-            for(int i=0;i<4;i++){
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                
-                if( nx >= 0 && ny >= 0 && nx < n && ny < m){
-                    if(!visited[nx][ny] && maps[nx].charAt(ny) != SEA){
-                        visited[nx][ny] = true;
-                        q.offer(new Point(nx,ny));
-                        sum += Integer.parseInt(String.valueOf(maps[nx].charAt(ny)));
-                    }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!visited[i][j] && !strMaps[i][j].equals(SEA)){
+                    list.add(bfs(i,j,visited,strMaps));
                 }
             }
-          }
-          return sum;
         }
+        if(list.size() == 0) return new int[]{-1};
         
+        Collections.sort(list);
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
+    
+    private static int bfs(int x, int y, boolean[][] visited, String[][] strMaps){
+        int n = strMaps.length, m = strMaps[0].length;
+        Queue<Point> q = new LinkedList<>();
+        q.offer(new Point(x,y));
+        visited[x][y] = true;
+        int count = Integer.parseInt(strMaps[x][y]);
+        
+        while(!q.isEmpty()){
+            Point now = q.poll();
+            
+            for(int i=0;i<4;i++){
+                int nx = now.x+dx[i];
+                int ny = now.y+dy[i];
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if(!visited[nx][ny] && !strMaps[nx][ny].equals(SEA)){
+                    count += Integer.parseInt(strMaps[nx][ny]);
+                    visited[nx][ny] = true;
+                    q.offer(new Point(nx, ny));
+                }
+            }
+        }
+        return count;
+    }
+}
