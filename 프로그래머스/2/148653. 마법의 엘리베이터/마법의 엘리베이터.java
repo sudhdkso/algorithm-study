cf028a79d1a25cd098126e99f3623bc15d7755c1
원지윤 <78727847+sudhdkso@users.noreply.github.com>
2025-07-28 22:46:06 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,19 +1,19 @@
 class Solution {
     public int solution(int storey) {
         int answer = 0;
-
-        while(storey != 0){
-            int nextNumber = (storey%100)/10;
-            int current = storey%10;
-            storey/=10;
-            if(current > 5 || (current == 5 && nextNumber >= 5)){
-                storey++;
-                answer += (10-current);
+        while(storey > 0){
+            int current = storey % 10;
+            storey /= 10;
+            int next = storey % 10;
+            
+            if(current > 5 || ( next >= 5 && current == 5)){
+                storey++;   
+                answer += 10 - current;
             }
-            else {  
-                answer += current;
+            else{
+                answer+= current;
             }
-
+            
         }
         return answer;
     }
