e280c360ab4073a2d69a96034eafe2c4985b9067
원지윤 <78727847+sudhdkso@users.noreply.github.com>
2025-08-04 01:36:21 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 111.95 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,41 +1,42 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+
 class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = {};
-        int kind = new HashSet<>(Arrays.asList(gems)).size();
-        Map<String, Integer> gemsMap = new HashMap<>();
+        int[] answer = new int[2];
+        List<String> list = Arrays.stream(gems).collect(Collectors.toList());
+        Set<String> set = new HashSet<>(list);
+        Map<String, Integer> map = new HashMap<>();
         
-        List<int[]> list = new ArrayList<>();
+        int kinds = set.size();
         int left = 0;
+        
         for(int i=0;i<gems.length;i++){
-            gemsMap.put(gems[i], gemsMap.getOrDefault(gems[i],0)+1);
+            map.put(gems[i], map.getOrDefault(gems[i], 0 )+1);
             
-            if(gemsMap.size() == kind){
-                while(gemsMap.get(gems[left]) > 1){
-                    gemsMap.put(gems[left], gemsMap.get(gems[left])-1);
+            if(map.size() == kinds){ 
+                while(map.get(gems[left]) > 1){
+                    map.put(gems[left], map.get(gems[left])-1);
                     left++;
                 }
-                
-                list.add(new int[]{left+1, i+1});
+                if(isMinRange(answer, new int[]{left+1, i+1})){
+                        answer = new int[]{left+1, i+1};
+                }
             }
-        }
-        return getMinSection(list);
-    }
-    
-    private int[] getMinSection(List<int[]> list){
-        int min = Integer.MAX_VALUE;
-        int[] answer = new int[2];
-        
-        for(int[] arr : list){
-            int len = arr[1] - arr[0];
-            if(min == len) continue;
-            if(min > len){
-                min = len;
-                answer = arr;
-            }
-           
         }
         return answer;
     }
-
+    
+    private static boolean isMinRange(int[] arr1, int[] arr2){
+        if(arr1[0] == 0 && arr1[1] == 0) return true;
+        int len1 = arr1[1] - arr1[0];
+        int len2 = arr2[1] - arr2[0];
+        
+        return len1 > len2;
+    }
 }
