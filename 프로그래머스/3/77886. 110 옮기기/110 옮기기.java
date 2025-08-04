32f2415f5f0d2e4073f528f89b8f57b1bd035e72
원지윤 <78727847+sudhdkso@users.noreply.github.com>
2025-08-05 00:03:21 +0900

    [level 3] Title: 110 옮기기, Time: 90.33 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public String[] solution(String[] s) {
         String[] answer = new String[s.length];
@@ -14,13 +15,11 @@ class Solution {
         
         for(char ch : s.toCharArray()){
             sb.append(ch);
+            
             int len = sb.length();
-            if(len >= 3){
-                String s1 = sb.substring(len-3, len);
-                if(s1.equals("110")){
-                    count++;
-                    sb.delete(len-3, len);
-                }
+            if(len >= 3 && sb.substring(len-3, len).equals("110")){
+               count++;
+                sb.delete(len-3, len);
             }
         }
         
