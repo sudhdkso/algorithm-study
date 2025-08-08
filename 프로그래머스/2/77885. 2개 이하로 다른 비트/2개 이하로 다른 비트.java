c7b7978daaa059467983efd64e3bd3683723ede7
원지윤 <78727847+sudhdkso@users.noreply.github.com>
2025-08-08 22:15:36 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 83.31 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,27 +1,25 @@
 class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
+        
         for(int i=0;i<numbers.length;i++){
-            if(numbers[i]%2 == 0){
-                answer[i] = numbers[i]+1;
-                continue;
-            }
-            StringBuilder binary = new StringBuilder(Long.toBinaryString(numbers[i]));
-            answer[i] = findNextNumber(binary);
+            if(numbers[i]%2 == 0)   answer[i] = numbers[i]+1;
+            else answer[i] = findByNextNumber(numbers[i]);
         }
         return answer;
     }
     
-    private long findNextNumber(StringBuilder binary){
-        int index = binary.toString().lastIndexOf("01");
+    private static Long findByNextNumber(long number){
+        StringBuilder binary = new StringBuilder(Long.toBinaryString(number));
+        int index = binary.lastIndexOf("01");
         if(index >= 0){
             binary.delete(index, index+2);
-            binary.insert(index, "10");
+            binary.insert(index, "10");            
         }
         else{
             binary.delete(0,1);
             binary.insert(0, "10");
         }
-        return Long.parseLong(binary.toString(), 2);
+        return Long.parseLong(binary.toString(),2);
     }
 }
