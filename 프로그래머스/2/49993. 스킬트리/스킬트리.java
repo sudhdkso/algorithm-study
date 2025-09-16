63b4f587ddb61627fa133bf53e153adc0a216af4
원지윤 <78727847+sudhdkso@users.noreply.github.com>
2025-09-16 20:40:45 +0900

    [level 2] Title: 스킬트리, Time: 0.09 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -2,27 +2,23 @@ class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
         
-        for(String skill_tree: skill_trees){
-            String rs = removeAnotherSkill(skill, skill_tree);
-            if(skill.indexOf(rs) == 0){
+        for(String skill_tree : skill_trees){
+            String s = remove(skill_tree, skill);
+            if(skill.indexOf(s) == 0){
                 answer++;
             }
         }
-        
         return answer;
     }
+
     
-    private static String removeAnotherSkill(String skill, String skill_tree){
+    private static String remove(String skill_tree, String skill){
         StringBuilder sb = new StringBuilder();
-        
         for(char ch : skill_tree.toCharArray()){
             if(skill.indexOf(ch) >= 0){
                 sb.append(ch);
             }
         }
         return sb.toString();
-        
     }
-    
-  
 }
