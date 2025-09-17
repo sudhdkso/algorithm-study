a32881fcb75965c12214a5fdb1a1d756041656cd
원지윤 <78727847+sudhdkso@users.noreply.github.com>
2025-09-17 22:50:43 +0900

    [level 3] Title: 거스름돈, Time: 15.90 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -3,18 +3,19 @@ import java.util.Arrays;
 class Solution {
     public int solution(int n, int[] money) {
         int answer = 0;
-        //money = {1,2,5}
         int[] dp = new int[n+1];
         
-        Arrays.sort(dp);
+        Arrays.sort(money);
         dp[0] = 1;
+        
         for(int i=0;i<money.length;i++){
-            for(int j=money[i]; j<=n; j++){
+            for(int j=money[i];j<=n;j++){
                 dp[j] += dp[j-money[i]];
                 dp[j] %= 1_000_000_007;
             }
         }
         
-        return dp[n];
+        answer = dp[n];
+        return answer;
     }
 }
