48ad9901fd9ed9c60bf3fea98efc86bb11824bb4
원지윤 <78727847+sudhdkso@users.noreply.github.com>
2025-09-18 23:24:54 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 130.44 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -5,32 +5,34 @@ import java.util.stream.Collectors;
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
         int answer = 0;
-        
         Queue<Integer> q1 = new LinkedList<>(Arrays.stream(queue1).boxed().collect(Collectors.toList()));
         Queue<Integer> q2 = new LinkedList<>(Arrays.stream(queue2).boxed().collect(Collectors.toList()));
+        long q1Sum = 0, q2Sum = 0;
+        for(int i=0;i<queue1.length;i++){
+            q1Sum += queue1[i];
+            q2Sum += queue2[i];
+        }
         
-        long q1sum = Arrays.stream(queue1).sum();
-        long q2sum = Arrays.stream(queue2).sum();
-        
-        while(q1sum != q2sum){
-            
-            if(queue1.length*3 < answer){
-              return -1;
+        while(q1Sum != q2Sum){
+            if(queue1.length * 3 < answer){
+                return -1;
             }
-            if(q1sum > q2sum){ 
-              int num = q1.poll();
-              q2.offer(num);
-              q1sum -= num;
-              q2sum += num;
+            if(q1Sum > q2Sum){
+                int num = q1.poll();
+                q2.offer(num);
+                q1Sum -= num;
+                q2Sum += num;
             }
-            else if(q2sum > q1sum){
+            else {
                 int num = q2.poll();
                 q1.offer(num);
-                q1sum += num;
-                q2sum -= num;
+                q1Sum += num;
+                q2Sum -= num;
             }
             answer++;
         }
+        
+        
         return answer;
     }
 }
